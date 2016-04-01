b7042c6d4d49280cced32fa951f927d8a43e5539
Q-u-y <qthoang@gmail.com>
2016-03-31 22:55:05 -0400

    blank project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Panda3::Application.config.secret_key_base = 'c84cc1130ac0a6ea25a9b1ae9e290c55b8e07d73c91c7f6e41d5b0bc191fdb6afe25a1fccca214e432225d86ed054ed4d695f72809006c77d3bcef780f5e9041'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Panda3::Application.config.session_store :cookie_store, key: '_Panda3_session'
